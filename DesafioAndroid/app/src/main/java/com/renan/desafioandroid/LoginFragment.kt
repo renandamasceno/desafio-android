c64056f5469118526d63e74c941d50eb61d62112
Renan <jrferreirad@gmail.com>
2020-11-08 22:54:29 -0300

    Implementa tela de login/register e navigation

---
+++ DesafioAndroid/app/src/main/java/com/renan/desafioandroid/LoginFragment.kt
@@ -0,0 +1,71 @@
+package com.renan.desafioandroid
+
+import android.os.Bundle
+
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+
+
+class LoginFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val navController = Navigation.findNavController(view)
+
+        view.findViewById<Button>(R.id.btnLogin).setOnClickListener {
+
+            val tilEmail = view.findViewById<TextInputLayout>(R.id.tilEmail)
+            val edtTextEmail = view.findViewById<TextInputEditText>(R.id.editTextEmail)
+
+            val tilPassword = view.findViewById<TextInputLayout>(R.id.tilPassword)
+            val edtTextPassword = view.findViewById<TextInputEditText>(R.id.editTextPassword)
+
+
+
+            if(edtTextEmail.text?.isEmpty()!!){
+                tilEmail.error = getString(R.string.emailError)
+            } else {
+                tilEmail.error = null
+            }
+
+            if(!validaPassword(edtTextPassword)){
+                tilPassword.error = getString(R.string.passwordError)
+            } else {
+                tilPassword.error = null
+            }
+
+
+        }
+
+        view.findViewById<Button>(R.id.btnRegister).setOnClickListener {
+            navController.navigate(R.id.registerFragment)
+        }
+    }
+
+    private fun validaPassword(text: TextInputEditText):Boolean {
+        if(text.text?.isEmpty()!!){
+            return false
+        } else return text.text?.length!! >= 8
+
+    }
+}
--- DesafioAndroid/app/src/main/java/com/renan/desafioandroid/MainActivity.kt
+++ DesafioAndroid/app/src/main/java/com/renan/desafioandroid/MainActivity.kt
@@ -2,10 +2,16 @@ package com.renan.desafioandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.isNotEmpty
+import com.google.android.material.textfield.TextInputLayout
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
 }
